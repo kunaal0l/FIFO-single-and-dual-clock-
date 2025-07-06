6df97ca38a84f72c67c862b0a901c8110a55e4b1
kunaal0l <165699290+kunaal0l@users.noreply.github.com>
2025-07-07 01:44:55 +0530

    Update fifo_single_clk.v

--- fifo_single_clk.v
+++ fifo_single_clk.v
@@ -1,10 +1,12 @@
 `timescale 1ns / 1ps
-module fifo_single_clk(clk,rst,buf_out,wr_en,rd_en,buf_empty,buf_full,buf_in,fifo_counter);
+module fifo_single_clk(clk,rst,buf_out,wr_en,rd_en,buf_empty,buf_full,buf_in,fifo_counter,fifo_threshold ,fifo_overflow,fifo_underflow);
 input rst,clk,wr_en,rd_en;
 input[7:0] buf_in;
 output[7:0] buf_out;
 output buf_empty,buf_full;//these are flags
 output [7:0] fifo_counter;
+output reg fifo_threshold;
+output reg fifo_overflow,fifo_underflow;
 reg [7:0] buf_out;
 reg buf_empty,buf_full;
 reg[7:0] fifo_counter;
@@ -13,6 +15,7 @@ reg[7:0] buf_mem[63:0];
 always @(fifo_counter)begin
     buf_empty=(fifo_counter==0);
     buf_full=(fifo_counter==64);
+    fifo_threshold=(fifo_counter>=32);
 end
 always @(posedge clk,posedge rst)begin
     if(rst)
@@ -25,6 +28,21 @@ always @(posedge clk,posedge rst)begin
         fifo_counter<=fifo_counter-1;
     else
         fifo_counter<=fifo_counter;    
+end
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        fifo_overflow  <= 0;
+        fifo_underflow <= 0;
+    end else begin
+        if (wr_en && buf_full)
+            fifo_overflow <= 1;
+        else if (rd_en)
+            fifo_overflow <= 0;
+        if (rd_en && buf_empty)
+            fifo_underflow <= 1;
+        else if (wr_en)
+            fifo_underflow <= 0;
+    end
 end
 always @(posedge clk) begin
     if(rst)
